df01cff5e1c2889a26da404b79f87cf495e16933
Martin Brand <m.brand96@gmx.de>
2023-06-07 11:38:02 +0200

    change boxplots

--- helper_functions/create.dep.score.boxplots.MSI.R
+++ helper_functions/create.dep.score.boxplots.MSI.R
@@ -13,12 +13,16 @@ create.dep.score.boxplots.MSI <- function(dep.scores,target.gene,class.table,
   boxplot.target.gene <- 
     ggplot(dep.scores.target.gene, aes(x=CCLE_MSI,y=dep.score)) +
     geom_boxplot() +
-    geom_jitter(shape=21,width=0.3,fill="darkgreen") +
-    stat_compare_means(method = "t.test") +
+    geom_jitter(shape=21,width=0.3,aes(fill=CCLE_MSI)) +
+    stat_compare_means(method = "wilcox.test",label.x=1.7) +
     ylab(paste(target.gene,"Dependency Score")) +
     scale_x_discrete(labels=c("MSS","MSI")) +
-    theme(legend.position="bottom") +
-    ggtitle(dataset)
+    theme(legend.position="none",
+          plot.title = element_text(hjust = 0.5),
+          text = element_text(size = 15),
+          axis.text = element_text(size = 10)) +
+    ggtitle(dataset) +
+    xlab("")
   
   return(boxplot.target.gene)
 }

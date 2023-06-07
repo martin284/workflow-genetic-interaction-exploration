dd5500d218f0d0ef30bfdcd6795f8a1009f51287
Martin Brand <m.brand96@gmx.de>
2023-06-07 11:28:56 +0200

    change boxplots

--- helper_functions/create.dep.score.boxplots.R
+++ helper_functions/create.dep.score.boxplots.R
@@ -1,5 +1,5 @@
 create.dep.score.boxplots <- function(dep.scores,target.gene,class.table,
-                                      dataset) {
+                                      dataset,pos.p.value=1.7) {
   dep.scores.target.gene <- dep.scores[c("CCLE_ID",target.gene)]
   dep.scores.target.gene <- merge(dep.scores.target.gene,
                                         class.table,by="CCLE_ID")
@@ -9,13 +9,17 @@ create.dep.score.boxplots <- function(dep.scores,target.gene,class.table,
   boxplot.target.gene <-
     ggplot(dep.scores.target.gene, aes(x=isMutated,y=dep.score)) +
     geom_boxplot() +
-    geom_jitter(shape=21,width=0.3,aes(fill=isDeleterious)) +
-    stat_compare_means(method = "t.test") +
+    geom_jitter(shape=21,width=0.3,aes(fill=isMutated)) +
+    stat_compare_means(method = "wilcox.test",label.x=pos.p.value) +
     ylab(paste(target.gene,"Dependency Score")) +
     scale_x_discrete(labels=c(paste0(genotype,"-non-mutated"),
                               paste0(genotype,"-mutated"))) +
-    theme(legend.position="bottom") +
-    ggtitle(dataset)
+    theme(legend.position="none",
+          plot.title = element_text(hjust = 0.5),
+          text = element_text(size = 15),
+          axis.text = element_text(size = 10)) +
+    ggtitle(dataset)  +
+    xlab("")
   
   return(boxplot.target.gene)
 }

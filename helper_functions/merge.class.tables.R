3a768f934546cea676a7f969410cd8b21ade2089
Martin Brand <m.brand96@gmx.de>
2023-06-19 16:37:28 +0200

    correct merging for del. classification

--- helper_functions/merge.class.tables.R
+++ helper_functions/merge.class.tables.R
@@ -2,6 +2,19 @@ merge.class.tables <- function(class.table.gene1, class.table.gene2) {
   class.table.comb <- merge(class.table.gene1,class.table.gene2,by="CCLE_ID",
                             all=T)
   
+  # keep cell lines with no NA and cell lines with at least one del. mutation
+  # otherwise cell lines that are mutated but non-deleterious stay not removed 
+  # correctly after merging
+  
+  class.table.comb <- 
+    class.table.comb[
+      (!is.na(class.table.comb$isDeleterious.x) &
+        !is.na(class.table.comb$isDeleterious.y)) | 
+        (is.na(class.table.comb$isDeleterious.x) &
+        class.table.comb$isDeleterious.y==T) |
+        (is.na(class.table.comb$isDeleterious.y) & 
+        class.table.comb$isDeleterious.x==T),]
+  
   class.table.comb$isDeleterious.x <- replace(
     class.table.comb$isDeleterious.x,is.na(class.table.comb$isDeleterious.x),F)
   class.table.comb$isDeleterious.y <- replace(

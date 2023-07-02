fc860354b8951947f5b9e851e8704edf5cf8ef33
Martin Brand <m.brand96@gmx.de>
2023-07-02 15:49:10 +0200

    clean-up code

--- helper_functions/create.dep.score.boxplots.MSI.R
+++ helper_functions/create.dep.score.boxplots.MSI.R
@@ -1,8 +1,13 @@
-create.dep.score.boxplots.MSI <- function(dep.scores,target.gene,class.table,
-                                          dataset,show.y.label=T) {
-  dep.scores.target.gene <- dep.scores[c("CCLE_ID",target.gene)]
+create.dep.score.boxplots.MSI <- function(dep.scores,
+                                          target.gene,
+                                          class.table,
+                                          dataset,
+                                          show.y.label = T) {
+  
+  # extract dependency scores for the selected gene
+  dep.scores.target.gene <- dep.scores[c("CCLE_ID", target.gene)]
   dep.scores.target.gene <- merge(dep.scores.target.gene,
-                                  class.table,by="CCLE_ID")
+                                  class.table, by = "CCLE_ID")
   dep.scores.target.gene <- na.omit(dep.scores.target.gene)
   colnames(dep.scores.target.gene)[2] <- "dep.score"
   
@@ -10,6 +15,7 @@ create.dep.score.boxplots.MSI <- function(dep.scores,target.gene,class.table,
   dep.scores.target.gene$CCLE_MSI <- factor(dep.scores.target.gene$CCLE_MSI,
                                             levels=c("MSS","MSI"))
   
+  # define if y-label is shown
   if(show.y.label==T) {
     y.label <- paste("Dependency Score for",target.gene)
   } else {
@@ -17,13 +23,13 @@ create.dep.score.boxplots.MSI <- function(dep.scores,target.gene,class.table,
   }
   
   boxplot.target.gene <- 
-    ggplot(dep.scores.target.gene, aes(x=CCLE_MSI,y=dep.score)) +
+    ggplot(dep.scores.target.gene, aes(x = CCLE_MSI, y = dep.score)) +
     geom_boxplot() +
-    geom_jitter(shape=21,width=0.3,aes(fill=CCLE_MSI)) +
-    stat_compare_means(method = "wilcox.test",label.x=1.7) +
+    geom_jitter(shape = 21, width = 0.3, aes(fill = CCLE_MSI)) +
+    stat_compare_means(method = "wilcox.test",label.x = 1.7) +
     ylab(y.label) +
-    scale_x_discrete(labels=c("MSS","MSI")) +
-    theme(legend.position="none",
+    scale_x_discrete(labels=c("MSS", "MSI")) +
+    theme(legend.position = "none",
           plot.title = element_text(hjust = 0.5),
           text = element_text(size = 15),
           axis.text = element_text(size = 10)) +
--- helper_functions/create.dep.score.boxplots.R
+++ helper_functions/create.dep.score.boxplots.R
@@ -1,31 +1,36 @@
 create.dep.score.boxplots <- function(dep.scores,target.gene,
                                       genotype,
                                       class.table,
-                                      dataset,pos.p.value=1.7,
+                                      dataset,pos.p.value = 1.7,
                                       sample.name.1 = "-non-mutated",
                                       sample.name.2 = "-mutated",
-                                      show.y.label=T) {
-  dep.scores.target.gene <- dep.scores[c("CCLE_ID",target.gene)]
-  dep.scores.target.gene <- merge(dep.scores.target.gene,
-                                        class.table,by="CCLE_ID")
+                                      show.y.label = T) {
+  
+  # extract dependency scores for the selected gene
+  dep.scores.target.gene <- dep.scores[c("CCLE_ID", target.gene)]
+  
+  # merge with classification table
+  dep.scores.target.gene <- merge(dep.scores.target.gene, class.table,
+                                  by = "CCLE_ID")
   dep.scores.target.gene <- na.omit(dep.scores.target.gene)
   colnames(dep.scores.target.gene)[2] <- "dep.score"
   
-  if(show.y.label==T) {
+  # define if y-label is shown
+  if(show.y.label == T) {
     y.label <- paste("Dependency Score for",target.gene)
   } else {
     y.label <- ""
   }
   
   boxplot.target.gene <-
-    ggplot(dep.scores.target.gene, aes(x=isMutated,y=dep.score)) +
+    ggplot(dep.scores.target.gene, aes(x = isMutated, y = dep.score)) +
     geom_boxplot() +
-    geom_jitter(shape=21,width=0.3,aes(fill=isMutated)) +
-    stat_compare_means(method = "wilcox.test",label.x=pos.p.value) +
+    geom_jitter(shape = 21, width = 0.3,aes(fill = isMutated)) +
+    stat_compare_means(method = "wilcox.test",label.x = pos.p.value) +
     ylab(y.label) +
-    scale_x_discrete(labels=c(paste0(genotype,sample.name.1),
-    paste0(genotype,sample.name.2))) +
-    theme(legend.position="none",
+    scale_x_discrete(labels = c(paste0(genotype, sample.name.1),
+    paste0(genotype, sample.name.2))) +
+    theme(legend.position = "none",
           plot.title = element_text(hjust = 0.5),
           text = element_text(size = 15),
           axis.text = element_text(size = 10)) +

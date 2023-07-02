90648ca45f3a49074a342f6fe6a3f0c58ef66d21
Martin Brand <m.brand96@gmx.de>
2023-07-02 09:49:42 +0200

    clean-up code

--- helper_functions/create.classification.table.expr.R
+++ helper_functions/create.classification.table.expr.R
@@ -1,49 +1,54 @@
-create.classification.table.expr <- function(sample.share,expression.class.gene,
+create.classification.table.expr <- function(sample.share, 
+                                             expression.class.gene,
                                              dep.scores.DRIVE,
                                              dep.scores.Achilles) {
-
-  expr.DRIVE <- merge(expression.class.gene,dep.scores.DRIVE["CCLE_ID"],
-                      by="CCLE_ID")
-  expr.DRIVE <- expr.DRIVE[order(expr.DRIVE$expr.level),]
   
+  # keep only cell lines that appear in expression dataset and DRIVE
+  expr.DRIVE <- merge(expression.class.gene, dep.scores.DRIVE["CCLE_ID"],
+                      by="CCLE_ID")
+  # order the cell lines for upcoming classification 
+  expr.DRIVE <- expr.DRIVE[order(expr.DRIVE$expr.level), ]
+  
+  # calculate the sample sizes resulting from the selected sample share
   sample.size.DRIVE <- round(sample.share * nrow(expr.DRIVE))
   
-  # filter out cell lines with highest and lowest expression in chosen gene
-  cell.lines.DRIVE.low.expr <- expr.DRIVE[1:sample.size.DRIVE,1]
+  # extract cell lines with highest and lowest expression in selected gene
+  cell.lines.DRIVE.low.expr <- expr.DRIVE[1:sample.size.DRIVE, 1]
   cell.lines.DRIVE.high.expr <- 
-    expr.DRIVE[(nrow(expr.DRIVE)-sample.size.DRIVE+1):nrow(expr.DRIVE),1]
+    expr.DRIVE[(nrow(expr.DRIVE)-sample.size.DRIVE+1):nrow(expr.DRIVE), 1]
   
   # create classification table
+  # to reuse code the variable is named isDeleterious again
+  # isDeleterious==T stands for low expression
   class.table.DRIVE.low.expr <- 
-    as.data.frame(cbind(CCLE_ID=cell.lines.DRIVE.low.expr,isDeleterious=F))
+    as.data.frame(cbind(CCLE_ID=cell.lines.DRIVE.low.expr, isDeleterious=F))
   class.table.DRIVE.high.expr <- 
-    as.data.frame(cbind(CCLE_ID=cell.lines.DRIVE.high.expr,isDeleterious=T))
+    as.data.frame(cbind(CCLE_ID=cell.lines.DRIVE.high.expr, isDeleterious=T))
   class.table.DRIVE <- 
-    rbind(class.table.DRIVE.low.expr,class.table.DRIVE.high.expr)
+    rbind(class.table.DRIVE.low.expr, class.table.DRIVE.high.expr)
   
-  rm(expr.DRIVE,cell.lines.DRIVE.low.expr,cell.lines.DRIVE.high.expr,
-     class.table.DRIVE.low.expr,class.table.DRIVE.high.expr)
+  # remove unnecessary objects
+  rm(expr.DRIVE, cell.lines.DRIVE.low.expr, cell.lines.DRIVE.high.expr,
+     class.table.DRIVE.low.expr, class.table.DRIVE.high.expr)
   
-  # same for Achilles 
-  expr.Achilles <- merge(expression.class.gene,dep.scores.Achilles["CCLE_ID"],
+  # repear the process for Achilles 
+  expr.Achilles <- merge(expression.class.gene, dep.scores.Achilles["CCLE_ID"],
                          by="CCLE_ID")
-  expr.Achilles <- expr.Achilles[order(expr.Achilles$expr.level),]
+  expr.Achilles <- expr.Achilles[order(expr.Achilles$expr.level), ]
   
   sample.size.Achilles <- round(sample.share * nrow(expr.Achilles))
   
-  # filter out cell lines with highest and lowest expression in chosen gene
-  cell.lines.Achilles.low.expr <- expr.Achilles[1:sample.size.Achilles,1]
+  cell.lines.Achilles.low.expr <- expr.Achilles[1:sample.size.Achilles, 1]
   cell.lines.Achilles.high.expr <- 
     expr.Achilles[
-      (nrow(expr.Achilles)-sample.size.Achilles+1):nrow(expr.Achilles),1]
+      (nrow(expr.Achilles) - sample.size.Achilles + 1):nrow(expr.Achilles), 1]
   
-  # create classification table
   class.table.Achilles.low.expr <- 
-    as.data.frame(cbind(CCLE_ID=cell.lines.Achilles.low.expr,isDeleterious=F))
+    as.data.frame(cbind(CCLE_ID=cell.lines.Achilles.low.expr, isDeleterious=F))
   class.table.Achilles.high.expr <- 
-    as.data.frame(cbind(CCLE_ID=cell.lines.Achilles.high.expr,isDeleterious=T))
+    as.data.frame(cbind(CCLE_ID=cell.lines.Achilles.high.expr, isDeleterious=T))
   class.table.Achilles <- 
-    rbind(class.table.Achilles.low.expr,class.table.Achilles.high.expr)
+    rbind(class.table.Achilles.low.expr, class.table.Achilles.high.expr)
   
-  return(list(DRIVE=class.table.DRIVE,Achilles=class.table.Achilles))
+  return(list(DRIVE=class.table.DRIVE, Achilles=class.table.Achilles))
 }
--- helper_functions/create_volcano_plot.R
+++ helper_functions/create_volcano_plot.R
@@ -1,24 +1,25 @@
-create_volcano_plot <- function(test_results, dataset, test_method,
-                                labeled_genes=NULL,
-                                show_sign_line=TRUE,
-                                show_y_label=TRUE){
-  mean_diffs_str <- paste0("mean_differences_",dataset)
+create_volcano_plot <- function(test_results,
+                                dataset,
+                                test_method,
+                                labeled_genes = NULL,
+                                show_sign_line = TRUE,
+                                show_y_label = TRUE) {  
+  
+  # extract mean differences and p-values for the given dataset
+  mean_diffs_str <- paste0("mean_differences_", dataset)
   mean_diffs <- test_results[[mean_diffs_str]]
-  p_adj_log_str <- paste0("p_values_",test_method,'_',dataset)
+  p_adj_log_str <- paste0("p_values_", test_method, '_', dataset)
   p_adj_log <- test_results[[p_adj_log_str]]['log']
   results <- cbind(p_adj_log, mean_diffs)
   results$gene <- rownames(results)
-
+  
+  # define labeled genes  
   results$to_label <- ""
-
   for (gene in labeled_genes) {
     results$to_label[results$gene == gene] <- gene
   }
   
-  test_method_UC <- paste(toupper(substr(test_method, 1, 1)),
-                          substr(test_method, 2, nchar(test_method)), sep="")
-  title <- paste0(dataset)
-  
+  # define if line representing significance threshold is shown
   if (show_sign_line) {
     sign_line <- -log10(0.05)
   }
@@ -26,20 +27,23 @@ create_volcano_plot <- function(test_results, dataset, test_method,
     sign_line <- NULL
   }
   
+  # define if y-label is shown
   if (show_y_label) {
     y_label <- expression(-log[10](P[adj]))
   } else {
     y_label <- ""
   }
   
-  result_plot <- ggplot(data=results, aes(x=D_ND, y=log, label=to_label)) +
-    geom_point(color="tomato3") +
-    geom_point(data = results[results$to_label != "",], color="black") +
+  # create result plot
+  result_plot <- 
+    ggplot(data = results, aes(x = D_ND, y = log, label = to_label)) +
+    geom_point(color = "tomato3") +
+    geom_point(data = results[results$to_label != "",], color = "black") +
     xlab('Mean difference') +
     ylab(y_label) +
     geom_label_repel(max.overlaps = Inf, nudge_y = 0.5) +
-    geom_hline(yintercept=sign_line) +
-    ggtitle(title) +
+    geom_hline(yintercept = sign_line) +
+    ggtitle(dataset) +
     theme(plot.title = element_text(hjust = 0.5),
           text = element_text(size = 15))
   
